37b11e43355aaa31fe12362edd7f050acda657a0
Charmaine <cnmogotlane@gmail.com>
2024-06-11 15:40:56 +0200

    starting the project

--- Persistence/obj/Debug/net8.0/Persistence.AssemblyInfo.cs
+++ Persistence/obj/Debug/net8.0/Persistence.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8658896506f16e940ddbf546d08d7786f07a7210")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d1a50f83b43b1bfe6d4dae5f488d3480324d20e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
