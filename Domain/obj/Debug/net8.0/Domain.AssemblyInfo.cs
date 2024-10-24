d084fc6b8fd4bbc20bbc189b850ea5a6c1a74267
Charmaine <cnmogotlane@gmail.com>
2024-10-24 09:43:36 +0200

    Project Complete!

--- Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
+++ Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4fc92dae8e13b72cd3fe829193d9dc3b843e7908")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e8cf8bb2f43bcd51f21b2f0e87ebe07f9940337d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
