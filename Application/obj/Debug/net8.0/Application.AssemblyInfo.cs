6756abb870cc2f92b7d13be0364b3b34bdbf6639
Charmaine <cnmogotlane@gmail.com>
2024-11-06 11:50:46 +0200

    Resolving build errors.

--- Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
+++ Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+781bd0b2e413300b86bf43d11f534bf0243e8f94")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41e60d87da15cda105be8424572bce8e3a41681f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
